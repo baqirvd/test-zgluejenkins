b9f24bf886a4876874f965d53247750581ff8908
Baqir <baqir@venturedive.com>
2019-10-16 20:36:43 +0500

    initial

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const a = 1;
+const b = a + 1;
+console.log(b);
+console.log(b);
+alert(a);
+alert(a);
+alert(a);
+alert(a);
+alert(a);
+console.log(b);
+console.log(b);
+console.log(b);
+console.log(b);
+console.log(b);
+console.log(b);
